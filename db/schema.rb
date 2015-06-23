ed057f4258394cae017d12a02b8dd1df78244b75
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-06-23 14:08:53 +0700

    admin create and update user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618011042) do
+ActiveRecord::Schema.define(version: 20150623062752) do
 
   create_table "activities", force: :cascade do |t|
     t.datetime "time"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20150618011042) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
