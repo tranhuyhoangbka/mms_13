ac93af147842c6d0b26e316f87f17d3da31393fb
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-06-23 09:20:21 +0700

    CRUD the Skills

---
+++ app/controllers/admin/skills_controller.rb
@@ -0,0 +1,47 @@
+class Admin::SkillsController < ApplicationController
+  before_action :admin_user
+  before_action :set_skill, only: [:edit, :update, :destroy]
+
+  def index
+    @skills = Skill.paginate page: params[:page], per_page: 
+                                   Settings.general.per_page
+    @skill = Skill.new                                   
+  end  
+
+  def create
+    @skill = Skill.new skill_params
+    if @skill.save
+      flash[:success] = t "skill.create_success"
+    else
+      flash[:danger] = t "skill.create_fail"
+    end
+    redirect_to admin_skills_url
+  end
+
+  def edit
+  end
+
+  def update
+    if @skill.update_attributes skill_params
+      flash[:success] = t "skill.update_success"
+    else
+      flash[:danger] = t "skill.update_fail"
+    end
+    redirect_to admin_skills_url
+  end
+
+  def destroy
+    @skill.destroy
+    flash[:success] = t "skill.delete_success"
+    redirect_to admin_skills_url
+  end
+
+  private
+  def skill_params
+    params.require(:skill).permit :name
+  end
+
+  def set_skill
+    @skill = Skill.find params[:id]
+  end
+end
