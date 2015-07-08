4fbd50a6ec91a6415a8a3b228c4ba822ed4b678e
hoang <hoangth92.nd@gmail.com>
2015-07-08 15:43:11 +0700

    test skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -5,7 +5,7 @@ class Skill < ActiveRecord::Base
   has_many :skill_users, dependent: :destroy
   has_many :users, through: :skill_users
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
 
   after_create :log_create
   after_update :log_update
